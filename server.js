070a1ee6c1c6d39fc12602acf0dc9cb75c75fcd3
Your Name <you@example.com>
2023-07-31 23:53:41 +0530

    updating the Auto Push

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ function mongoConnect() {
 if (process.env.DOCUMENTDB == 'true') {
 function mongoConnect() {
     return new Promise((resolve, reject) => {
-    var mongoURL = process.env.MONGO_URL || 'mongodb://username:password@mongodb:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
+    var mongoURL = process.env.MONGO_URL || 'mongodb://admin1:roboshop1@roboshop-dev-docdb.cluster-cdrcc2ywouls.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
     var client = mongoClient.connect(mongoURL,
       {
         // Mutable & Immutable
